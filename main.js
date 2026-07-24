b5674bb005022b6651d5e0e2a71fd3fac5d7f7f3
MD HABIBOR RAHMAN HIRA <68804057+mdhira-ai@users.noreply.github.com>
2026-07-24 16:33:41 -0400

    feat: add logging for autoUpdater events and update dependencies; bump version to 1.0.2 in package.json

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("node:path");
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
+// --- Add logging so you can actually see what's happening ---
+log.transports.file.level = 'info';
+autoUpdater.logger = log;
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -13,7 +17,7 @@ const createWindow = () => {
       preload: path.join(__dirname, "./preload/preload.js"),
     },
     roundedCorners: true,
-    autoHideMenuBar:true,
+    autoHideMenuBar: true,
     maximizable: false,
   });
 
@@ -23,9 +27,33 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow();
 
-   autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
+
+// --- Add these so you can see every stage of the update process ---
+autoUpdater.on('checking-for-update', () => {
+  log.info('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+  log.info('Update available:', info.version);
+});
+autoUpdater.on('update-not-available', () => {
+  log.info('No update available (already on latest version)');
+});
+autoUpdater.on('error', (err) => {
+  log.error('Update error:', err);
+});
+autoUpdater.on('download-progress', (progress) => {
+  log.info(`Download progress: ${progress.percent.toFixed(1)}%`);
+});
+autoUpdater.on('update-downloaded', () => {
+  log.info('Update downloaded, will install on quit');
+});
+
+ipcMain.handle("app:get-version", () => {
+  return app.getVersion();
+});
 ipcMain.handle("app:get-version", () => {
   return app.getVersion();
 });
